31da493144c1551dfa9e50e0b45595b0ef190903
suwdle <suwdle1917@gmail.com>
2024-10-16 09:28:36 +0900

    five

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
 
-    public static void main(String[] args) {
-      System.out.println("송석준");
-      World.run();
-    }
-  
+  public static void main(String[] args) {
+    System.out.println("송석준");
+    World.run();
+    World.run2();
   }
-  
+
+}
